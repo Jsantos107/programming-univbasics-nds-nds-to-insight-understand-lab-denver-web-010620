f3d21c941e43f2f3920c9917e3e8f79d318a3453
Jorge Santos <neneluis55@gmail.com>
2019-12-27 02:38:46 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,7 +10,7 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  row_index = 0
+  row_index += 1
  director_data = directors_database[0][:movies]
 while row_index < director_data.length do
   puts director_data[row_index][:title]
