b5e6c29cb21ed991360b22e5abe2f5ef7a80f3c0
Jorge Santos <neneluis55@gmail.com>
2019-12-27 02:31:49 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,6 +11,6 @@ end
 
 def print_first_directors_movie_titles
   row_index = 0
-while row_index < print_first_directors_movie_titles.length do
-  puts "Row #{row_index} has #{print_first_directors_movie_titles[row_index]} columns"
+while row_index < directors_database.length do
+  puts 
 end
